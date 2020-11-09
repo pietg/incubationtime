d5cc3a8ce23d9082bd0762c5b989383285b53ec9
Piet Groeneboom <P.Groeneboom@tudelft.nl>
2020-11-09 10:15:34 +0100

    update continuous model

---
+++ simulations_continuous_model.R
@@ -0,0 +1,13 @@
+# Simulations of the variances of the density estimate
+# for 1000 samples of n=1000 for a model of the
+# distribution the incubation time
+
+ library(Rcpp)
+ sourceCpp("continuous_model.cpp")
+ output <- simulation()
+ 	
+	
+	
+	
+
+    
