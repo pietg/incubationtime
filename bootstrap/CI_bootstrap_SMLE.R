fbea84778342fef366c45cccc588f2c4e58a18c7
Piet Groeneboom <p.groeneboom@tudelft.nl>
2021-11-08 22:21:08 +0100

    bootstrap SMLE

--- bootstrap/CI_bootstrap_SMLE.R
+++ bootstrap/CI_bootstrap_SMLE.R
@@ -10,6 +10,6 @@ B <- output$CI_df
 
 pdf("CI_SMLE.pdf")
 plot(c(-100,-100),xlim=c(0,12), ylim=c(0,max(y3)), main= "",ylab="",xlab="",bty="n",las=1)
-lines(x1,y1,lwd=2,type ="s",col="red")
+lines(x1,y1,lwd=2,col="red")
 segments(x1,y2,x1,y3)
 dev.off()
